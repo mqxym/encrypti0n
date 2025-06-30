254396ea30e01db125eb77a63fc9ac91c0e6abdc
mQxYm <maxim@omg.lol>
2025-06-30 17:50:16 +0200

    3.0.0b8-3: Migrated inline styles and scripts
    Code Structure:
    - Custom CSS stylesheet: Removed inline styles in favor of a custom.css stylesheet
    - Removed inline scripts and moved logic to insertVersion.js
    
    UI Changes:
    - Added 'new' tag for data-decoder.html in side menu

--- app/insertVersion.js
+++ app/insertVersion.js
@@ -5,3 +5,14 @@ const appVersion = "3.0.0b7";
 document.querySelectorAll(".version").forEach(function(el) {
   el.textContent = appVersion;
 });
+
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+document.addEventListener('DOMContentLoaded', (event) => { 
+    const highlightElements = document.querySelectorAll('.language-javascript, .language-json');
+    if (highlightElements.length > 0) {
+        highlightElements.forEach((el) => { 
+            hljs.highlightElement(el); 
+        });
+    }
+});
